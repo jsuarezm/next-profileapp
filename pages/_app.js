504745f1e3ede153f643dcd6adfd37d19cbce1f6
Jesus Suarez <jesus.a.suarez@gmail.com>
2021-04-18 01:38:19 +0200

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { Amplify } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css';
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({...awsExports, ssr: true });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>)
+};
 
 export default MyApp
